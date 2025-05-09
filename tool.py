14b3d54546c970b4420e019f8913609be68815d9
Quitto <brunamoreiraquitto@gmail.com>
2025-05-09 18:34:44 -0300

    1.0v Lauch

--- tool.py
+++ tool.py
@@ -18,8 +18,10 @@ class tool:
         try:
             subprocess.run(["git","init"])
             subprocess.run(["git", "branch", "-M", "main"], check=True)
-            subprocess.run(["git", "remote", "remove", "origin"], check=True)
-            subprocess.run(["git", "remote", "add", "origin", data.remote_link], check=True)
+            #git pull origin main --allow-unrelated-histories
+            subprocess.run(["git","pull","origin","main","--allow-unrelated-histories"]) #remove o commit que nao foi puchado
+            subprocess.run(["git", "remote", "remove", "origin"], check=True) #Remove coneçao para evitar erros
+            subprocess.run(["git", "remote", "add", "origin", data.remote_link], check=True) #Adiciona A coneçao remote novamente
             subprocess.run(["git", "add", "."], check=True)
             subprocess.run(["git", "commit", "-m", data.commit], check=True)
             subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
